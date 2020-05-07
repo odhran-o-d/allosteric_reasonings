3dced80feb5daa8b61e5cd6a56a9042e73334cca
odhran-o-d <39832722+odhran-o-d@users.noreply.github.com>
2020-05-07 13:04:29 +0100

    randomiser switched to sklearn
    not yet tested

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,10 +1,11 @@
 import torch
 import numpy as np
+from sklearn.utils import shuffle
 
 
 def load_data(data, batch_number):
-    np.random.shuffle(data)
-    objects, relationships, subjects = np.split(data, 3, axis=1)
+    shuffled_data = shuffle(data)
+    objects, relationships, subjects = np.split(shuffled_data, 3, axis=1)
     batched = lambda x: np.array_split(x, batch_number)
     # if np.array_split isn't deterministic you may have different size splits!
     batched_objects = batched(objects)
