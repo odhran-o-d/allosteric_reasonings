a16f8a52fb3dbf8276bf6c667028d35ea74fc7fb
odhran-o-d <39832722+odhran-o-d@users.noreply.github.com>
2020-05-08 11:08:03 +0100

    created sparse vectors but currently encoutering memory error

--- utils/loaders.py
+++ utils/loaders.py
@@ -12,3 +12,14 @@ def load_data(data, batch_number):
     batched_subjects = batched(subjects)
     batched_relationships = batched(relationships)
     return batched_objects, batched_subjects, batched_relationships
+
+
+def load_data_for_BCE(data, batch_number):
+    shuffled_data = shuffle(data)
+    objects, relationships, subjects = np.split(shuffled_data, 3, axis=1)
+    batched = lambda x: np.array_split(x, batch_number)
+    # if np.array_split isn't deterministic you may have different size splits!
+    batched_objects = batched(objects)
+    batched_subjects = batched(subjects)
+    batched_relationships = batched(relationships)
+    return batched_objects, batched_subjects, batched_relationships
